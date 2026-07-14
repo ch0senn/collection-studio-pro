e7ae28ce56d7ab45b4f58f31d0872d576e32a3ff
ch0senn <141234614+ch0senn@users.noreply.github.com>
2026-07-14 15:50:59 +0100

    Add pre_load.js to expose collectionStudio version

---
+++ electron/pre_load.js
@@ -0,0 +1,9 @@
+const { contextBridge } = require("electron");
+
+contextBridge.exposeInMainWorld("collectionStudio", {
+    version: "0.3.0"
+});const { contextBridge } = require("electron");
+
+contextBridge.exposeInMainWorld("collectionStudio", {
+    version: "0.3.0"
+});
