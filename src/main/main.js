4e872205b0af85cc354c04074d14f726e2dd154f
ch0senn <141234614+ch0senn@users.noreply.github.com>
2026-07-14 18:13:20 +0100

    Initialize Electron app with main window setup

---
+++ src/main/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1400,
+        height: 900,
+        minWidth: 1000,
+        minHeight: 700,
+        backgroundColor: "#111827",
+        title: "Collection Studio Pro",
+        webPreferences: {
+            contextIsolation: true,
+            nodeIntegration: false
+        }
+    });
+
+    win.loadFile(path.join(__dirname, "../renderer/index.html"));
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
